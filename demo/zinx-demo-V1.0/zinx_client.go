acb8ed0d00c190cb27169ed7589222b1a2a0e427
樊川 <945001786@qq.com>
2022-08-08 22:18:56 +0800

    no

--- demo/zinx-demo-V1.0/zinx_client.go
+++ demo/zinx-demo-V1.0/zinx_client.go
@@ -22,7 +22,7 @@ func main() {
 	buf := make([]byte, 512)
 	for {
 		//向服务端发送数据
-		_, err = conn.Write([]byte("Hello Zinx..."))
+		_, err = conn.Write([]byte("Hello Zinx this is a Connection!"))
 		if err != nil {
 			fmt.Println("client write data err", err)
 			return
